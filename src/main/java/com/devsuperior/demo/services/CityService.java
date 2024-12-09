fe9d3ef7342ef85116b981181104b6b3eb547902
thomaskavi <thomaskavi2000@gmail.com>
2024-12-09 13:56:21 -0300

    city get sorted by name

---
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -0,0 +1,24 @@
+package com.devsuperior.demo.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.demo.dto.CityDTO;
+import com.devsuperior.demo.entities.City;
+import com.devsuperior.demo.repositories.CityRepository;
+
+@Service
+public class CityService {
+
+  @Autowired
+  private CityRepository repository;
+
+  public List<CityDTO> findAll() {
+    List<City> list = repository.findAll(Sort.by("name"));
+    return list.stream().map(CityDTO::new).collect(Collectors.toList());
+  }
+}
