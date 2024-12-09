06ca90ba4b72f3247820d36943577e3a8cb89bec
thomaskavi <thomaskavi2000@gmail.com>
2024-12-09 14:07:02 -0300

    city post, integration test insert and find all passing

--- src/main/java/com/devsuperior/demo/services/CityService.java
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.demo.dto.CityDTO;
 import com.devsuperior.demo.entities.City;
@@ -17,8 +18,17 @@ public class CityService {
   @Autowired
   private CityRepository repository;
 
+  @Transactional(readOnly = true)
   public List<CityDTO> findAll() {
     List<City> list = repository.findAll(Sort.by("name"));
     return list.stream().map(CityDTO::new).collect(Collectors.toList());
+  }
+
+  @Transactional
+  public CityDTO insert(CityDTO dto) {
+    City entity = new City();
+    entity.setName(dto.getName());
+    entity = repository.save(entity);
+    return new CityDTO(entity);
   }
 }
